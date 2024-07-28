c94b7d57cafb956edcb2b9b4540b8246f1a1d2cd
Artem101990 <72793619+Artem101990@users.noreply.github.com>
2024-07-28 22:42:00 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+my_string = input('Введите Ваше Имя и Фамилию: ')
+print('Колличество символов', len(my_string), )
+print('В этой строке верхний регистр', my_string .upper(), )
+print('В этой строке нижний регистр', my_string .lower(), )
+print('В этой строке нет пробелов', my_string.replace(' ', ''))
+print('Вывод первого символа строки', my_string[0], )
+print('Вывод последнего символа строки', my_string[-1], )
+
+  
