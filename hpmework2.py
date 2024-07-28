bc26d68fd897b3eef83a5445e968b875a3fc67b0
Artem101990 <72793619+Artem101990@users.noreply.github.com>
2024-07-28 15:33:22 +0300

    Add files via upload

---
+++ hpmework2.py
@@ -0,0 +1,6 @@
+#homework2.py#
+task ='12'
+work_time  ='1.5'
+Name_Course ='Python'
+time_one_task = '0.125'
+print('Курс:',Name_Course, ', всего задач:', task, ', потрачено часов:', work_time, ', среднее время выполнения:', time_one_task)
