5638ab1ff15ff93bbcefc3f90cd8a09b40ab8378
Artem101990 <72793619+Artem101990@users.noreply.github.com>
2024-07-30 20:42:43 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 'Alex' , 5, 8, True
+print('immutable_var:',  immutable_var)
+#immutable_var [1]=2
+#print(immutable_var)
+# Значения списка переменной обьявленных изначально не могут поменять структуру ,потому что данные нельзя перезаписать)
+mutable_list = ['Strong', 8, 6]
+mutable_list[0:1]= 'List'
+print ('mutable_list:',  mutable_list)
